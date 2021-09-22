665ecdf41c887f733d0d86d8dd096e45b255516b
Chase Murtaugh <cmurtau@g.clemson.edu>
2021-09-22 10:23:23 -0400

    Randomly selects a movie, checks if it loads, if not, reloads

--- js/movies.js
+++ js/movies.js
@@ -1,8 +1,10 @@
-var sampleQuery = "https://api.themoviedb.org/3/movie/550?api_key=2be50216a9231d782c1ba136d60ba871" //550 is what I assume the movie number is, randomize that and we got it random
-
 var movieTitle = $('#title')
 var moviePlot = $('#plot')
 var posterDisplay = $('#poster')
+
+var movieID = Math.floor((Math.random() * 10000) + 1);
+
+var sampleQuery = "https://api.themoviedb.org/3/movie/" + movieID + "?api_key=2be50216a9231d782c1ba136d60ba871" //550 is what I assume the movie number is, randomize that and we got it random
 
 
 
@@ -14,11 +16,17 @@ function queryMovieDB() {
         })
         .then(function (data) {
         	console.log(data)
+		if(data.status_code == 34 || data.poster_path == null){
+			console.log("working")
+			window.location.href = window.location.href;
+		}
+		else{
 			movieTitle.text(data.original_title)
 			moviePlot.text(data.overview)
 			var moviePoster = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2" + data.poster_path
 			posterDisplay.empty()
 			posterDisplay.append("<img src='"+ moviePoster + "'></img>")
+		}
 		
 		}
         
@@ -26,3 +34,8 @@ function queryMovieDB() {
         )};
 
 		queryMovieDB()
+
+
+
+
+console.log(movieID)
