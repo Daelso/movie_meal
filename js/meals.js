ae818179b5ef6c8a62fb05e6f9638273eace8c4d
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-09-22 11:37:30 -0400

    appending images and text of food data

--- js/meals.js
+++ js/meals.js
@@ -1,12 +1,37 @@
-function mealsFetchAPI() {
-    var requestMealsUrl = 'https://api.spoonacular.com/recipes/complexSearch';
+var APIKey = "759f66f279b4468e91990ad761aa5966"
+var queryUrl = "https://api.spoonacular.com/food/products/search?query=chicken" + "&apiKey=" + APIKey;
+// var entry = "burgers"
 
-    fetch(mealsFetchAPI)
+var mealTitle = $('#title')
+var mealImage = $('#image')
+
+
+
+
+    fetch(queryUrl)
         .then(function (response) {
         return response.json();
-        //console.log(response)
         })
         .then(function (data) {
         console.log(data)
+        mealTitle.text(data.products[2].title)
+        // mealImage.append(data.products[2].image)
+
+        var foodImage = data.products[2].image
+			mealImage.empty()
+			mealImage.append("<img src='"+ foodImage + "'></img>")
         });
-}
+
+        
+    
+
+
+
+        // if(data.status == 34 || data.image_path == null){
+		// 	console.log("working")
+		// 	window.location.href = window.location.href;
+        
+        // }
+        // else {
+        // }
+
