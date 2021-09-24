46ba4ae2bb9f8bb49c3ea5072f4c5658fe84b1a3
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-09-24 15:38:53 -0400

    merge into index.js and logic

--- js/index.js
+++ js/index.js
@@ -20,6 +20,13 @@ var storeTitle = JSON.parse(localStorage.getItem('movie_title')) || [];
 
 var storeIMDB = JSON.parse(localStorage.getItem("imdb_link")) || [];
 
+var mealTitle = $('#food-title')
+var mealImage = $('#image')
+var recipeIngredients = $('#cuisineRecipe')
+
+
+var APIKey = "759f66f279b4468e91990ad761aa5966"
+// var cuisineType = "chinese"
 
 
 
@@ -208,5 +215,75 @@ function chooseGenreID(event) {
 	var genreID = e.value
 	getGenreList(genreID)
 
+	var f = document.getElementById("cuisine-search");
+	var cuisineType = f.value
 
+	recipeIngredients.empty()
+	queryCuisineDB(cuisineType)
+
+
+	
+	
 }
+
+
+function queryCuisineDB(meals){
+	if (meals) {
+		var cuisineType = meals
+	}
+	var cuisineUrl = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + cuisineType + "&apiKey=" + APIKey;
+
+    fetch(cuisineUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+    
+        
+        //have the recipe of the meal selected display as well
+
+        var randomArray = Math.floor((Math.random() * 10) + 1)
+        mealTitle.text(data.results[randomArray].title)
+        mealImage.append(data.results[randomArray].image)
+        var foodImage = data.results[randomArray].image
+			mealImage.empty()
+			mealImage.append("<img src='"+ foodImage + "'></img>")
+
+        var recipeOption = data.results[randomArray].id
+        console.log(recipeOption)
+        
+        recipeIdTransfer (recipeOption)
+
+
+         // 
+
+        });
+}
+
+    function recipeIdTransfer (recipeId) {
+        if (recipeId) {
+            var mealId = recipeId
+        }
+
+        var getRecipeUrl = "https://api.spoonacular.com/recipes/" + mealId + "/ingredientWidget.json?&apiKey=" + APIKey
+
+        fetch (getRecipeUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            console.log(data.ingredients[0].name);
+    
+            for (var i= 0; i < data.ingredients.length; i++) {
+                var ingredientsText = $('<p>').text(data.ingredients[i].name)
+                recipeIngredients.append(ingredientsText)         
+            }
+        });
+
+    }
+
+    queryCuisineDB();
+
+
