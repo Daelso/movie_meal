2c79b919021f03a7221a580221e5da28f79e2314
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-09-24 14:59:16 -0400

    add recipe logic and display it to new div

--- js/meals.js
+++ js/meals.js
@@ -1,8 +1,10 @@
 var APIKey = "759f66f279b4468e91990ad761aa5966"
-var queryChickenUrl = "https://api.spoonacular.com/food/products/search?query=chicken" + "&apiKey=" + APIKey;
-var cuisineUrl = "https://api.spoonacular.com/recipes/complexSearch?cuisine=italian" + "&apiKey=" + APIKey;
+var cuisineType = "chinese"
+var cuisineUrl = "https://api.spoonacular.com/recipes/complexSearch?cuisine=" + cuisineType + "&apiKey=" + APIKey;
 var mealTitle = $('#title')
 var mealImage = $('#image')
+var recipeIngredients = $('#cuisineRecipe')
+
 
 function queryCuisineDB(){
     fetch(cuisineUrl)
@@ -11,17 +13,48 @@ function queryCuisineDB(){
         })
         .then(function (data) {
             console.log(data)
+    
         
-        mealTitle.text(data.results[2].title)
-        // mealImage.append(data.results[2].image)
+        //have the recipe of the meal selected display as well
 
-        var foodImage = data.results[2].image
+        var randomArray = Math.floor((Math.random() * 10) + 1)
+        mealTitle.text(data.results[randomArray].title)
+        mealImage.append(data.results[randomArray].image)
+        var foodImage = data.results[randomArray].image
 			mealImage.empty()
 			mealImage.append("<img src='"+ foodImage + "'></img>")
+
+        var cuisineOption = data.results[randomArray].id
+        console.log(cuisineOption)
+        
+        cuisineIdTransfer (cuisineOption)
+
+
+         // 
+
         });
 }
+
+    function cuisineIdTransfer (cuisineId) {
+        if (cuisineId) {
+            var mealId = cuisineId
+        }
+
+        var getRecipeUrl = "https://api.spoonacular.com/recipes/" + mealId + "/ingredientWidget.json?&apiKey=" + APIKey
+
+        fetch (getRecipeUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            console.log(data.ingredients[0].name);
+    
+        recipeIngredients.text(data.ingredients[0].name)
+        });
+
+    }
+
     queryCuisineDB();
 
-//randomize it
-//put it in a button
-//do the same for recipes
+
